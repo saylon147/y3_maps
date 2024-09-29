3b917e74c25658f439fb08ab1b7a85b71a0cdc38
saylon147 <saylon@thebingo.cc>
2024-09-29 09:52:22 +0800

    又试了一下练功房的demo，同样也是有点问题的，考虑还是从头来写一下自己的逻辑算了。

--- empty_with_y3/maps/EntryMap/script/main.lua
+++ empty_with_y3/maps/EntryMap/script/main.lua
@@ -22,52 +22,39 @@ end
 -- end)
 
 
-local battle_wave = require 'y3.演示.demo.防守图.刷兵'
-local game_result = require 'y3.演示.demo.防守图.游戏结果'
+local monster_wave = require 'y3.演示.demo.练功房.刷怪'
 
 y3.ltimer.wait(0.5, function()
-    -- 为玩家1创建主控英雄并选中
-    local hero = y3.player(1):create_unit(134274912, y3.point.create(0, 0, 0), 180.0)
+    -- 给玩家1创建主控英雄并选中
+    local hero = y3.player(1):create_unit(134242543, y3.point.create(0, 0, 0), 180.0)
     y3.player(1):select_unit(hero)
 
-    -- 让英雄强一点
+    -- 设置英雄属性
     hero:set_level(10)
     hero:add_attr('物理攻击', 70)
     hero:add_attr('法术攻击', 150)
     hero:add_attr('攻击速度', 200)
     hero:add_attr('物理吸血', 20)
-    hero:add_attr('暴击率', 20)
-    hero:add_attr('技能吸血', 200)
-    hero:add_attr('魔法恢复', 10)
+    hero:add_attr('暴击率', 80)
+    hero:add_attr('技能吸血', 500)
+    hero:add_attr('魔法恢复', 20)
     hero:add_attr('冷却缩减', 50)
 
-    -- 玩家英雄死亡，游戏失败
-    hero:event('单位-死亡', function()
-        game_result.lose()
-        battle_wave.stop()
-    end)
-end)
+    -- 创建练功房区域
+    local circle_area = y3.area.create_circle_area(y3.point.create(-1000, -1000, 0), 800)
 
--- 等待2秒后开始刷兵
-y3.ltimer.wait(2, function()
-    battle_wave.next_wave()
-    -- 第一次出兵后，每15秒再出一次兵
-    y3.ltimer.loop(15, function()
-        battle_wave.next_wave()
+    -- 当英雄进入练功房时开始刷怪
+    circle_area:event('区域-进入', function(trg, data)
+        if data.unit:is_hero() then
+            monster_wave.start()
+        end
     end)
-end)
 
-y3.ltimer.loop(1, function(timer)
-    -- 如果场上的怪超过30个，判定游戏失败
-    if battle_wave.get_alive_count() >= 30 then
-        game_result.lose()
-        battle_wave.stop()
-        timer:remove()
-    end
-    -- 如果场上没有怪了，也没有下一波怪了，判定游戏胜利
-    if battle_wave.get_alive_count() == 0 and not battle_wave.has_next() then
-        game_result.win()
-        battle_wave.stop()
-        timer:remove()
-    end
+    -- 1.当英雄离开练功房5秒后删除区域内怪物
+    -- 2.如果5秒内英雄折返则不删除
+    circle_area:event('区域-离开', function(trg, data)
+        if data.unit:is_hero() then
+            monster_wave.delete(circle_area, 5)
+        end
+    end)
 end)
