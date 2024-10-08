cad50a4f4205bc9bc23ceb53a58cf356ff58cb1c
saylon147 <saylon@thebingo.cc>
2024-10-08 16:00:27 +0800

    添加游戏开始后创建主角，以及创建敌对建筑，并且每隔3秒建筑物刷新一个小怪。

--- test_game_002/maps/EntryMap/script/states/maingame.lua
+++ test_game_002/maps/EntryMap/script/states/maingame.lua
@@ -2,6 +2,12 @@ local maingame = {}
 
 function maingame:enter()
     print("进入 MAINGAME")
+
+    -- 创建主角
+    local hero = y3.player(1):create_unit(134219010, y3.point.create(0, 0, 0), 180.0)
+
+    -- 创建刷怪点
+    local building = y3.unit.create_unit(y3.player(31), 134237546, y3.point.create(-1000, -1000, 0), 0.0)
 end
 
 function maingame:update()
