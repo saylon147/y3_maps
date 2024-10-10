d1e1753a1026e0e2bafaf55903b6ae2de1da5d0b
dingjunjie <503459792@qq.com>
2024-10-10 17:58:15 +0800

    去除无用代码

--- bzkd/bzkd/maps/EntryMap/script/main.lua
+++ bzkd/bzkd/maps/EntryMap/script/main.lua
@@ -17,7 +17,6 @@ end)
 
 y3.timer.loop(5, function (timer, count)
     print('每5秒显示一次文本，这是第' .. tostring(count) .. '次')
-    FW.uiMgr:hideUI('rolePanel')
 end)
 
 y3.game:event('键盘-按下', y3.const.KeyboardKey['SPACE'], function ()
