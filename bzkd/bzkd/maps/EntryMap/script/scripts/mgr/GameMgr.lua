72f3fb9712ec14e237a07562b89fbf366fd1ac23
dingjunjie <503459792@qq.com>
2024-10-11 18:10:19 +0800

    下一轮按钮

--- bzkd/bzkd/maps/EntryMap/script/scripts/mgr/GameMgr.lua
+++ bzkd/bzkd/maps/EntryMap/script/scripts/mgr/GameMgr.lua
@@ -19,6 +19,7 @@ function M:gameStart()
     -- end,"生成随机水晶",true)
     FW.globalVar["gameState"] = FW.const.gameState['gaming']
     FW.playerMgr:initPlayerUnits()
+    self:initGlobalEvent()
     self:roundEnemy()
 end
 
@@ -34,14 +35,26 @@ function M:createRandomCrystal()
 end
 
 function M:roundEnemy()
-    y3.timer.loop(FW.const.enemyBornTimeOut,function (timer,count)
-        if count * FW.const.enemyBornTimeOut >= FW.const.roundTime then
-            FW.unitMgr:killAllEnemy()
-            timer:remove()
-        end
+    y3.timer.loop(FW.const.enemyBornTimeOut, function(timer, count)
         for key, value in pairs(FW.playerMgr.allPlayers) do
-            FW.unitMgr:createRandomUnitAtRandomPoint(y3.player.get_by_id(31),"enemy",FW.const.enemyRandomArea[key],0,FW.const.enemyCount,FW.playerMgr:getHeroByPlayer(value))
+            FW.unitMgr:createRandomUnitAtRandomPoint(y3.player.get_by_id(31), "enemy", FW.const.enemyRandomArea[key], 0,
+                FW.const.enemyCount, FW.playerMgr:getHeroByPlayer(value))
         end
+        if count * FW.const.enemyBornTimeOut >= FW.const.roundTime then
+            timer:remove()
+            FW.unitMgr:killAllEnemy()
+            y3.ui.get_ui(FW.playerMgr.allPlayers[1],'Runtime.nextround'):set_visible(true)
+        end
+    end)
+end
+
+function M:initGlobalEvent()
+    local nextBtn = y3.ui.get_ui(FW.playerMgr.allPlayers[1],'Runtime.nextround')
+    nextBtn:add_event('左键-按下', '开启下一轮')
+    nextBtn:set_visible(false)
+    y3.game:event('界面-消息', '开启下一轮', function(trg, data)
+        nextBtn:set_visible(false)
+        M:roundEnemy()
     end)
 end
 
