7f21a8866c75e40ea1c355d17c735738caa9c99b
saylon147 <saylon@thebingo.cc>
2024-10-15 14:23:04 +0800

    调通角色创建以及刷怪点的创建

--- test_game_002/maps/EntryMap/script/states/maingame.lua
+++ test_game_002/maps/EntryMap/script/states/maingame.lua
@@ -4,17 +4,22 @@ function maingame:enter()
     print("进入 MAINGAME")
 
     -- 创建主角
-    local player = y3.player.get_by_handle(GameAPI.get_client_role())
-    local hero = player:create_unit(134219010, y3.point.create(0, 0, 0), 180.0)
-    hero:event('单位-击杀', function(trg, data)
-        print("击杀目标 " .. data.unit:get_name())
-        -- GameManager.player_kill = GameManager.player_kill + 1
-    end)
-    hero:event('单位-死亡', function(trg, data)
-        self:show_result("lose")
-    end)
+    for i = 1, 4 do
+        local player = y3.player.get_by_id(i)
+        if player:get_state() == 1 then
+            local hero = y3.unit.create_unit(player, 134219010, y3.point.create(0, 0, 0), 180.0)
+            hero:event('单位-击杀', function(trg, data)
+                player:display_info("击杀目标 " .. data.unit:get_name())
+                -- GameManager.player_kill = GameManager.player_kill + 1
+            end)
+        end
+    end
 
-    -- -- 创建刷怪点
+    -- hero:event('单位-死亡', function(trg, data)
+    --     self:show_result("lose")
+    -- end)
+
+    -- 创建刷怪点
     local building = y3.unit.create_unit(y3.player(31), 134237546, y3.point.create(-1000, -1000, 0), 0.0)
 end
 
