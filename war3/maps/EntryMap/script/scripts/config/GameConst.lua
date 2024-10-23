cecada56fc2652c06d0c4554c86036a4ec144d10
dingjunjie <503459792@qq.com>
2024-10-23 17:19:15 +0800

    修改了下物编

--- war3/maps/EntryMap/script/scripts/config/GameConst.lua
+++ war3/maps/EntryMap/script/scripts/config/GameConst.lua
@@ -10,10 +10,16 @@ gameConst.gameState = {
     result = 5,         --游戏结算
 }
 gameConst.bornPoint = {
-    y3.point.get_point_by_res_id(10000),
-    y3.point.get_point_by_res_id(10001),
-    y3.point.get_point_by_res_id(10002),
-    y3.point.get_point_by_res_id(10003)
+    y3.point.get_point_by_res_id(10008),
+    y3.point.get_point_by_res_id(10009),
+    y3.point.get_point_by_res_id(10010),
+    y3.point.get_point_by_res_id(10011)
+}
+gameConst.enemyBornPoint = {
+    y3.point.get_point_by_res_id(10012),
+    y3.point.get_point_by_res_id(10013),
+    y3.point.get_point_by_res_id(10014),
+    y3.point.get_point_by_res_id(10015)
 }
 -- gameConst.playerCamera = {
 --     y3.camera.create_camera(y3.point.create(0,0,0),3000,35,0,56,10000),
