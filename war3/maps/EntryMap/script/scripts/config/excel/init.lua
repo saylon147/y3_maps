6b6da45a6c7cb878f8b00c3a2c4127ef598cda14
dingweikang <johnjoe002@163.com>
2024-10-30 09:45:57 +0800

    更新物编

--- war3/maps/EntryMap/script/scripts/config/excel/init.lua
+++ war3/maps/EntryMap/script/scripts/config/excel/init.lua
@@ -1,5 +1,6 @@
 local config = {
 brushMonster = require 'scripts.config.excel.brushMonster',
+global = require 'scripts.config.excel.global',
 unit = require 'scripts.config.excel.unit',
 }
 return config
