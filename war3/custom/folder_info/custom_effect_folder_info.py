f134181cd3feaf00e7c04d8f2970c91019f2ed86
huangxiaofeng <k1624@qq.com>
2024-10-30 15:18:27 +0800

    提交技能

--- war3/custom/folder_info/custom_effect_folder_info.py
+++ war3/custom/folder_info/custom_effect_folder_info.py
@@ -2,4 +2,4 @@
 _reload_all = True
 
 
-data = {'f': [('', 0, 2147483647, 'code_explorer_custom_root_folder_name'), ('/2147483647', 0, 'Nh-XNJUgEe+7tsWjX1X8Ia', '1')], 'd': {}}
+data = {'f': [('', 0, 2147483647, 'code_explorer_custom_root_folder_name'), ('/2147483647', 0, 'Nh-XNJUgEe+7tsWjX1X8Ia', '1')], 'd': {134279282: (2147483647, 2)}}
